b43866ebe3396761ae2b74ef4a2ed7eceeda05b5
Tooma81 <toomas.kaarel@gmail.com>
2025-06-26 11:23:00 +0300

    Added price formatting to EUR

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,13 +1,16 @@
 const MealItem = (props) => {
     const { name, price, description, image } = props.meal;
 
+    const formattedPrice = new Intl.NumberFormat("de-DE", {style: "currency",
+        currency: "EUR"}).format(price)
+
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${image}`)} alt={name} />
                 <h3>{name}</h3>
                 <p className="meal-item-description">{description}</p>
-                <p className="meal-item-price">${price}</p>
+                <p className="meal-item-price">{formattedPrice}</p>
                 <div className="meal-item-actions">
                     <button className="button">Add to Cart</button>
                 </div>
